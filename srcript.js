f665505e0798791c865838652f13533686c40ac6
mahee414 <75530975+mahee414@users.noreply.github.com>
2021-07-13 21:17:09 +0530

    Update srcript.js

--- srcript.js
+++ srcript.js
@@ -1,1 +1,39 @@
-Hh
+// Created by Mahesh maddi
+
+// for addition 
+function a()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x+y; document.getElementById("g").value=z ;
+} 
+//for subtraction
+function b()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x-y; 
+  document.getElementById("g").value=z;
+} 
+//for multiplication
+function c()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x*y; 
+  document.getElementById("g").value=z ;
+} 
+//for division 
+function d()
+{ 
+var x=parseInt(document.getElementById("e").value);
+ var y=parseInt(document.getElementById("f").value);
+ var z=x/y; 
+  document.getElementById("g").value=z ;
+} 
+//clear button 
+function clearIt() { 
+document.getElementById('e').value = "";
+document.getElementById('f').value = "";
+ document.getElementById('g').value = "";
+}
